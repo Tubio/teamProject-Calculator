3a69249d63ee56fe151421a51ff33897a59e5cfa
Tubio <mtubio96@gmail.com>
2018-08-15 15:25:37 -0300

    Basic Service

---
+++ src/main/java/com/example/demo/services/CalculatorService.java
@@ -0,0 +1,28 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+	
+	public Double getResult(Double number1, Double number2, String operation) {
+		
+		Double result;
+		
+		if		(operation == "+")
+			result = number1 + number2;
+		
+		else if (operation == "-")
+			result = number1 - number2;
+		
+		else if (operation == "*")
+			result = number1 * number2;
+		
+		else if (operation == "/")
+			result = number1 / number2;
+		
+		else result = null; //invalid operation
+		
+		return result;
+	}
+}
