18c6919a13b0b184e2712ddb1cbaa1a3a0548d49
DarioJahnel <dariojahnel@gmail.com>
2018-08-21 10:45:34 -0300

    Indentation correction

--- src/test/java/com/example/demo/CalculatorServiceCaseTest.java
+++ src/test/java/com/example/demo/CalculatorServiceCaseTest.java
@@ -15,74 +15,74 @@ public class CalculatorServiceCaseTest extends TestCase{
 		
 	CalculatorService calculatorService = new CalculatorService();
         
-        @BeforeEach
-        public void showTestName(TestInfo testInfo){
-        System.out.println("Beggining test: " + testInfo.getDisplayName()  );
-        }
+    @BeforeEach
+    public void showTestName(TestInfo testInfo){
+    	System.out.println("Beggining test: " + testInfo.getDisplayName()  );
+    }
        
 	@Test
-        @DisplayName("Test Sum")
+    @DisplayName("Test Sum")
 	public void testSum() {
-		
-            Queue<String> mock = new LinkedList<String>();
-            mock.addAll(Arrays.asList("10","+","20","+","30"));
-            
-            Double sumResult = calculatorService.operate(mock);
-            assertEquals(60d,sumResult);
+	
+        Queue<String> mock = new LinkedList<String>();
+        mock.addAll(Arrays.asList("10","+","20","+","30"));
+        
+        Double sumResult = calculatorService.operate(mock);
+        assertEquals(60d,sumResult);
 	}
 	@RepeatedTest(10) //this allows the test to be repeated 10 times
-        @DisplayName("Repeated Test example")
-        public void repeatedSumTestExample(RepetitionInfo repetitionInfo){
-			Queue<String> mock = new LinkedList<String>();
-	        mock.addAll(Arrays.asList("10","+","20","+","30"));
-	        
-	        Double sumResult = calculatorService.operate(mock);
-	        assertEquals(60d,sumResult);
-	        }
+    @DisplayName("Repeated Test example")
+    public void repeatedSumTestExample(RepetitionInfo repetitionInfo){
+		Queue<String> mock = new LinkedList<String>();
+        mock.addAll(Arrays.asList("10","+","20","+","30"));
         
-        @Test
-        @DisplayName("Test Substraction")
+        Double sumResult = calculatorService.operate(mock);
+        assertEquals(60d,sumResult);
+        }
+        
+    @Test
+    @DisplayName("Test Substraction")
 	public void testSub() {
 		
-            Queue<String> mock = new LinkedList<String>();
-            mock.addAll(Arrays.asList("10","-","20","-","30"));
-            
-            Double subResult = calculatorService.operate(mock);
-            assertEquals(-40d,subResult);
+        Queue<String> mock = new LinkedList<String>();
+        mock.addAll(Arrays.asList("10","-","20","-","30"));
+        
+        Double subResult = calculatorService.operate(mock);
+        assertEquals(-40d,subResult);
 	}
 	
-        @Test
-        @DisplayName("Test Product")
+    @Test
+    @DisplayName("Test Product")
 	public void testProd() {
 	
-            Queue<String> mock = new LinkedList<String>();
-            mock.addAll(Arrays.asList("1","*","10","*","10"));
-            
-            Double prodResult = calculatorService.operate(mock);
-            assertEquals(100d,prodResult);
+        Queue<String> mock = new LinkedList<String>();
+        mock.addAll(Arrays.asList("1","*","10","*","10"));
+        
+        Double prodResult = calculatorService.operate(mock);
+        assertEquals(100d,prodResult);
 	}
 	
-        @Test
-        @DisplayName("Test Division")
+    @Test
+    @DisplayName("Test Division")
 	public void testDiv() {
 		
-            Queue<String> mock = new LinkedList<String>();
-            mock.addAll(Arrays.asList("20","/","2","/","10"));
-            
-            Double divResult = calculatorService.operate(mock);
-            assertEquals(1d,divResult);
+        Queue<String> mock = new LinkedList<String>();
+        mock.addAll(Arrays.asList("20","/","2","/","10"));
+        
+        Double divResult = calculatorService.operate(mock);
+        assertEquals(1d,divResult);
 	}	
         
-        @Test
-        @DisplayName("Test Division by zero")
-        public void testDivByZero(){
+    @Test
+    @DisplayName("Test Division by zero")
+    public void testDivByZero(){
             
-            Queue<String> mock = new LinkedList<String>();
-            mock.addAll(Arrays.asList("5","/","0"));
-           
-            assertThrows(ArithmeticException.class,
-                    ()->{
-                        calculatorService.operate(mock);
-                    });
-        }
+        Queue<String> mock = new LinkedList<String>();
+        mock.addAll(Arrays.asList("5","/","0"));
+       
+        assertThrows(ArithmeticException.class,
+                ()->{
+                    calculatorService.operate(mock);
+                });
+    }
 }
